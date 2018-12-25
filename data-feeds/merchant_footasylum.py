c05f70cda19b0a9047736c25b3a304168c42fc09
Dawud Rahman <dawudr@gnmail.com>
2018-12-25 15:19:25 +0000

    First commit

---
+++ data-feeds/merchant_footasylum.py
@@ -0,0 +1,82 @@
+import csv
+
+class Uhp:
+
+    def replace_category(self, input) :
+        x = input.split(",")
+        output = ""
+
+        count_level = 0
+        for y in x:
+            # We only interested in 2 sub categories
+            if count_level < 2:
+                if output == "":
+                    # First Category
+                    # Check if categories key exists so we can replace from dictionary
+                    if y in category_dict:
+                        output += category_dict[y]
+                    else:
+                        # Break loop
+                        return "Uncategorized"
+                else:
+                    # 2nd or more categories
+                    if y in category_dict:
+                        output += ", " + category_dict[y]
+                    # else Should we add as a new category here?
+                count_level += 1
+        return (output)
+
+
+
+category_dict = {'Womens': 'Ladies',
+                 'Womens Footwear': 'Shoes and Bags',
+                 'Womens Accessories': 'Accessories',
+                 'Womens Clothing': 'Clothing',
+                 'Kids': 'Children',
+                 'Junior Clothing': 'Clothing',
+                 'Kids Accessories': 'Everything Else',
+                 'Kids Footwear': 'Everything Else',
+                 'Nursery Clothing': 'Everything Else',
+                 'Mens': 'Mens',
+                 'Mens Accessories': 'Accessories',
+                 'Mens Clothing': 'Clothing',
+                 'Mens Footwear': 'Shoes'
+                 }
+uhp = Uhp()
+
+data = [];
+with open('datafeed_525247.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        print(f'Processing item line: {line_count+1} - name: {row[1]} rrp price: {row[21]} sale price: {row[7]}')
+        if line_count == 0:
+            # print(f'Column names are {", ".join(row)}')
+            # aw_deep_link, product_name, merchant_product_id, merchant_image_url, description, merchant_category, merchant_name, merchant_id, search_price, rrp_price
+            # cols = (f'{row[0]}, {row[1]}, {row[3]}, {row[4]}, {row[5]}, {row[6]}, {row[8]}, {row[9]}, {row[7]}, {row[21]}, uhp_category')
+            # Woocommerce Columns for import
+            # External URL, Name, ID, Images, Description, x, x, x, Sale price, Regular price, Categories
+            # cols = (f'External URL, Name, SKU, Images, Description, {row[6]}, {row[8]}, {row[9]}, Sale price, Regular price, Categories, Type')
+            cols = (f'Name, SKU, Images, Short Description, {row[6]}, {row[8]}, {row[9]}, Sale price, Regular price, Categories, External URL, Type')
+            # print(f'Column names are {(cols)}')
+            data.append(cols)
+        else:
+            if float(row[7]) <= float(row[21]) / 2:
+                # print(f'HALF PRICE..... {row[0]}, {row[1]}, {row[3]}, {row[4]}, {row[5]}, {row[6]}, {row[8]}, {row[9]}, {row[7]}, {row[21]}')
+                # aw_deep_link, product_name, merchant_product_id, merchant_image_url, description, merchant_category, merchant_name, merchant_id, search_price, rrp_price
+                # item = (f'"{row[0]}", "{row[1]}", "{row[3]}", "{row[4]}", "{row[5]}", "{row[6]}", "{row[8]}", "{row[9]}", {row[7]}, {row[21]}') + ", \"" + uhp.replace_category(row[6]) + "\"" + ", \"external\""
+                item = (f'"{row[5]}", "{row[3]}", "{row[4]}", "{row[1]}", "{row[6]}", "{row[8]}", "{row[9]}", {row[7]}, {row[21]}, "{uhp.replace_category(row[6])}", "{row[0]}", \"simple\"')
+                data.append(item)
+        line_count += 1
+
+    print(f'\nProcessed {int(line_count) -1} lines items. \r\n Under Half Price Items found: {len(data) -1}')
+
+with open( 'datafeed_uhp_output.csv', "w") as csv_file:
+    # writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONE, escapechar=' ')
+    for line in data:
+        # print(line)
+        csv_file.write(line )
+        csv_file.write('\n')
+    # writer.writerows([data])
+
+    print(f'Output written to file: datafeed_uhp_output.csv')
